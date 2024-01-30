5a0a524aca7d480fecb34b6696ee679d6a6cf4d8
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2024-01-30 14:58:31 -0500

    Initial Commit

---
+++ Musie me/Musie_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  Musie_meApp.swift
+//  Musie me
+//
+//  Created by Angelo Brown on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Musie_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
